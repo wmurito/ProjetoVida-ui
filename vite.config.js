7b2f707ab12493ebf28cecc265a22ea44d0862d2
wmurito <wellington_murito@hotmail.com>
2025-10-26 16:37:54 -0300

    fix: Replace terser with esbuild minifier

--- vite.config.js
+++ vite.config.js
@@ -24,13 +24,7 @@ export default defineConfig(({ mode }) => {
       }
     },
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
+    minify: 'esbuild',
     chunkSizeWarningLimit: 1000
   },
   server: {
